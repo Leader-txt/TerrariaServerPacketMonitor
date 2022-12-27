a3861ddd7e6996753dd0a1973baa06252baf3f23
Leader <3251263056@qq.com>
2022-12-27 12:32:40 +0800

    upload files

---
+++ TerrariaServerPacketMonitor/Analyze.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariaServerPacketMonitor
+{
+    internal class Packet
+    {
+        public string Name { get; set; }
+        public PacketTypes Type { get; set; }
+        public long Count { get; set; }
+        public long TotalBytes { get; set; }
+    }
+    internal class Analyze
+    {
+        public long TotalPackets { get; set; }
+        public long TotalBytes { get; set; }
+        public long ReceivePackets { get; set; }
+        public long ReceiveBytes { get; set; }
+        public long SendPackets { get; set; }
+        public long SendBytes { get; set; }
+        public Packet[] Send { get; set; }
+        public Packet[] Receive { get; set; }
+    }
+}
---
+++ TerrariaServerPacketMonitor/MianPlugin.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace TerrariaServerPacketMonitor
+{
+    [ApiVersion(2, 1)]
+    public class MianPlugin : TerrariaPlugin
+    {
+        public override string Author => "Leader";
+        public override string Description => "数据包监控";
+        public override string Name => "TerrariaServerPacketMonitor";
+        public override Version Version => new Version(1, 0, 0, 0);
+        internal List<Packet> Receive { get; set; } = new List<Packet>();
+        internal List<Packet> Send { get; set; } = new List<Packet>();
+        public MianPlugin(Main game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.NetSendBytes.Register(this, OnNetSendBytes);
+            ServerApi.Hooks.NetGetData.Register(this, OnNetGetData);
+            Commands.ChatCommands.Add(new Command("", analyze, "ana"));
+        }
+
+        private void analyze(CommandArgs args)
+        {
+            //c
+            {
+                Receive.Sort((x, y) => y.Count.CompareTo(x.Count));
+                Send.Sort((x, y) => y.Count.CompareTo(x.Count));
+                var text = JsonConvert.SerializeObject(new Analyze()
+                {
+                    TotalPackets = Receive.Select(x => x.Count).Sum() + Send.Select(x => x.Count).Sum(),
+                    TotalBytes = Receive.Select(x => x.TotalBytes).Sum() + Send.Select(x => x.TotalBytes).Sum(),
+                    Send = Send.ToArray(),
+                    SendBytes = Send.Select(x => x.TotalBytes).Sum(),
+                    SendPackets = Send.Select(x => x.Count).Sum(),
+                    Receive = Receive.ToArray(),
+                    ReceiveBytes = Receive.Select(x => x.TotalBytes).Sum(),
+                    ReceivePackets = Receive.Select(x => x.Count).Sum()
+                }, Formatting.Indented);
+                File.WriteAllText("analyze_c.txt", text);
+            }
+            {//
+                Receive.Sort((x, y) => y.TotalBytes.CompareTo(x.TotalBytes));
+                Send.Sort((x, y) => y.TotalBytes.CompareTo(x.TotalBytes));
+                var text = JsonConvert.SerializeObject(new Analyze()
+                {
+                    TotalPackets = Receive.Select(x => x.Count).Sum() + Send.Select(x => x.Count).Sum(),
+                    TotalBytes = Receive.Select(x => x.TotalBytes).Sum() + Send.Select(x => x.TotalBytes).Sum(),
+                    Send = Send.ToArray(),
+                    SendBytes = Send.Select(x => x.TotalBytes).Sum(),
+                    SendPackets = Send.Select(x => x.Count).Sum(),
+                    Receive = Receive.ToArray(),
+                    ReceiveBytes = Receive.Select(x => x.TotalBytes).Sum(),
+                    ReceivePackets = Receive.Select(x => x.Count).Sum()
+                }, Formatting.Indented);
+                File.WriteAllText("analyze_b.txt", text);
+            }
+            args.Player.SendSuccessMessage("数据包收发分析表已生成");
+        }
+
+        private void OnNetGetData(GetDataEventArgs args)
+        {
+            var data = Receive.Find(x => x.Type == args.MsgID);
+            if ( data== null)
+            {
+                Receive.Add(new Packet() { Type = args.MsgID, Count = 1, TotalBytes = args.Msg.totalData, Name = args.MsgID.ToString() });
+            }
+            else
+            {
+                data.Count++;
+                data.TotalBytes += args.Msg.totalData;
+            }
+        }
+
+        private void OnNetSendBytes(SendBytesEventArgs args)
+        {
+            var type = (PacketTypes)args.Buffer[2];
+            var data = Send.Find(x => x.Type == type);
+            if (data == null)
+            {
+                Send.Add(new Packet() { Type = type, Count = 1, TotalBytes = args.Count, Name = type.ToString() }); 
+            }
+            else
+            {
+                data.Count++;
+                data.TotalBytes += args.Count;
+            }
+        }
+    }
+}
